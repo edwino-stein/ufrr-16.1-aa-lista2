3b7981144738d4b4f7475513ff076c421d3b7b49
Edwino Stein <edwino.stein@gmail.com>
2016-08-05 11:05:06 -0400

    Adicionando Common-C

---
+++ common/common.h
@@ -0,0 +1,19 @@
+/**
+ *  Desenvolvido por Edwino Stein - edwino.stein@gmail.com
+ */
+
+#if !defined  _COMMON_BASE
+#define _COMMON_BASE
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h> //Define o tipo booleano
+
+    #include "header/string.h"
+    #include "header/util.h"
+    #include "header/stdOutput.h"
+    #include "header/stdInput.h"
+    #include "header/console.h"
+    #include "header/random.h"
+
+#endif
